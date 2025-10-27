c4d05d8bc47b4d0f5764153ec7d6e8a63397eba1
Elena Chustova <chustova.lena@gmail.com>
2025-10-27 23:40:39 +0300

    make / and /articles views class

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from hexlet_django_blog.article import views
+from .views import Article_home_page
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', Article_home_page.as_view()),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from hexlet_django_blog.views import HomePageView
 # Create your views here.
-def index(request):
-    return render(
-        request,
-        'articles/index.html',
-        context={'app_name': 'Article'}
-    )
+
+class Article_home_page(HomePageView):
+    template_name = 'articles/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["app_name"] = "Article"
+        return context
